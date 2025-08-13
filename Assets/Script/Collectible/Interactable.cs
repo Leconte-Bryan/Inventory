719d763f7698c7ec3c5fa2a13e73bb1bbb1bca7f
VilainRouquin <bryan.leconte.escalona@gmail.com>
2025-08-14 01:19:16 +0200

    Make Item move and rotate
    Rotate over y axis
    sinusoide movement over x and y axis

--- Assets/Script/Collectible/Interactable.cs
+++ Assets/Script/Collectible/Interactable.cs
@@ -6,7 +6,33 @@ public class Interactable : MonoBehaviour
     public ItemType itemType;
     [SerializeField] public bool affectOnlyPlayer;
     [SerializeField] AudioClip interactionSound;
+    [SerializeField] float rotatingSpeed = 50f;
 
+    Vector3 originalPos;
+
+    [Header("Sinusoide")]
+    [SerializeField] float yValue; // Updated value
+    [SerializeField] float xValue; // Updated value
+    [SerializeField] float sinusoideSpeed = 3; // Speed at wich the sinusoide goes (the frequency)
+    [SerializeField] float amplitude = 0.5f; // augment maximum and minimum
+
+
+    private void Start()
+    {
+        originalPos = transform.position; // Stock the original position of the object
+    }
+
+    private void Update()
+    {
+        // current value of the sinusoide (will oscile between amplitude & -amplitude)
+        yValue = originalPos.y + Mathf.Sin(Time.time * sinusoideSpeed) * amplitude;
+        // current value of the sinusoide (will oscile between amplitude & -amplitude)
+        // xValue = originalPos.x + Mathf.Cos(Time.time * sinusoideSpeed) * amplitude;
+        //float sinusoideValueY = Mathf.Clamp(originalPos.y + yValue, originalPos.y - amplitude, originalPos.y + amplitude);
+        gameObject.transform.Rotate(0, rotatingSpeed * Time.deltaTime, 0);
+        // Apply sinusoide to the y axis (add the sinusoide value to the originalPos.y)
+        gameObject.transform.position = new Vector3(originalPos.x, yValue, originalPos.z);
+    }
 
     public virtual void PickUpAction(GameObject target)
     {
